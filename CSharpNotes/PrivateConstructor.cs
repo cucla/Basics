287fca723872f8f8cc5bc5454efe420b621903c3
cucla <cucla@users.noreply.github.com>
2018-09-26 17:04:00 +0800

    Update PrivateConstructor.cs

--- CSharpNotes/PrivateConstructor.cs
+++ CSharpNotes/PrivateConstructor.cs
@@ -6,7 +6,8 @@
  */ 
 public class MyClass
 {
-    private MyClass(object data1, string data2) { }   
+    private MyClass(object data1, string data2) { } 
+    
     public MyClass(object data1) : this(data1, null) { }
     public MyClass(string data2) : this(null, data2) { }
     public MyClass() : this(null, null) { }
@@ -19,14 +20,27 @@ public class MyClass
 public abstract class BaseClass
 {
     private BaseClass() { }
-    public class SubClass1 : BaseClass
-    {
-        public SubClass1() : base() { }
-    }
+    
+    public class SubClass1 : BaseClass { public SubClass1() : base() { } }
+    public class SubClass2 : BaseClass { public SubClass2() : base() { } }
+}
 
-    public class SubClass2 : BaseClass
+/* 3. Factory
+ * When a static function is used to obtain an instance of the class 
+ * rather than explicit instantiation
+ */ 
+ public class MyClass
+{ 
+    private static Dictionary<object, MyClass> cache = new Dictionary<object, MyClass>();
+
+    private MyClass() { }
+
+    public static MyClass GetInstance(object data)
     {
-        public SubClass2() : base() { }
+        MyClass output;
+        if(!cache.TryGetValue(data, out output)) 
+            cache.Add(data, output = new MyClass());
+
+        return output;           
     }
 }
- 
