e02624742b9b4a8f0c1f1526d8fc9ebd50040d96
cucla <cucla@users.noreply.github.com>
2018-09-26 16:47:11 +0800

    Update PrivateConstructor.cs

--- CSharpNotes/PrivateConstructor.cs
+++ CSharpNotes/PrivateConstructor.cs
@@ -3,14 +3,30 @@
 /* 1. Base Constructor
  * Can be used to create "base" constructors that are called from other
  * more accessible constructors
- */
- 
- public class MyClass
+ */ 
+public class MyClass
 {
     private MyClass(object data1, string data2) { }   
     public MyClass(object data1) : this(data1, null) { }
     public MyClass(string data2) : this(null, data2) { }
     public MyClass() : this(null, null) { }
 }
- 
+
+/* 2. Pseudo-Sealed with Nested Children
+ * Any nested classes that inherit from the outer class can access the 
+ * private constructor
+ */ 
+public abstract class BaseClass
+{
+    private BaseClass() { }
+    public class SubClass1 : BaseClass
+    {
+        public SubClass1() : base() { }
+    }
+
+    public class SubClass2 : BaseClass
+    {
+        public SubClass2() : base() { }
+    }
+}
  
