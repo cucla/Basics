76f6cfe757eef7037c4494bb6ae990265343c149
cucla <cucla@users.noreply.github.com>
2018-09-27 16:58:23 +0800

    Update element_destruction_order.cpp

--- Stack/Cplusplus_Stack/element_destruction_order.cpp
+++ Stack/Cplusplus_Stack/element_destruction_order.cpp
@@ -14,7 +14,7 @@ using my_stack = std::stack<T, my_deque<T>>;
 
 /* 2. provide your own implementation of stack whose destructor pops all the elements:
  */
- template <class T, class Container = std::deque<T>>
+template <class T, class Container = std::deque<T>>
 struct ordered_stack : std::stack<T, Container>
 {
     using std::stack<T, Container>::stack;
