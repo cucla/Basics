fe1bdf1a61c15d35f8479b3b216e90d9548347ec
cucla <cucla@users.noreply.github.com>
2018-09-28 17:55:51 +0800

    Update Templates_tutorial.cpp

--- CPPNotes/Templates_tutorial.cpp
+++ CPPNotes/Templates_tutorial.cpp
@@ -40,6 +40,7 @@ int main()
 
 
 /* Understand template type deduction
+ *
  * ================================================
  * template<typename T>
  * void f(ParamType param);	// (ex.   void f(const T& param);   )
@@ -48,12 +49,20 @@ int main()
  * ================================================
  * Need to deduce two types: one for T and one for ParamType (includes const, references...)
  * Type of T depends on expr and ParamType, and we have 3 cases:
- * ================================================
+ *
  * Case 1: ParamType is a Reference or Pointer, but not a Universal Reference
+ * ================================================
  * template<typename T>
  * void f(T& param);      
  * ------------------------------------------------
  * f(x);                   // int x = 27;		T is int, param's type is int&
  * f(cx);                  // const int cx = 27;	T is const int, param's type is const int&
  * f(rx);                  // const int& rx = 27;	T is const int, param's type is const int&, T is deduced to be a non-reference
+ * ================================================
+ * template<typename T>
+ * void f(const T& param);      
+ * ------------------------------------------------
+ * f(x);                   // int x = 27;		T is int, param's type is const int&
+ * f(cx);                  // const int cx = 27;	T is int, param's type is const int&
+ * f(rx);                  // const int& rx = 27;	T is int, param's type is const int&
  * ================================================
