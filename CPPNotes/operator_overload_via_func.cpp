06356dfac9ced09f3812366e5dfdea8a51a1996e
cucla <cucla@users.noreply.github.com>
2018-11-02 11:26:48 +0800

    Create operator_overload_via_func.cpp

---
+++ CPPNotes/operator_overload_via_func.cpp
@@ -0,0 +1,20 @@
+class A {
+public:
+	A( const int & a ) : a(a) {}
+	const int & value() const { return a; }
+private:
+	int a;
+};
+
+int operator +(const A & lhs, const A & rhs) {
+	return lhs.value() + rhs.value();
+}
+
+int main() 
+{
+	A one(7);
+	A two(10);
+	std::cout <<"one + two: " << one + two << std::endl;
+
+	std::cin.get();
+}
