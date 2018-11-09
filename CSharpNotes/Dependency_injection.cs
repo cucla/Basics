a8e516d18db7bf8455588543d5ccb9be0ac5a899
cucla <cucla@users.noreply.github.com>
2018-11-09 11:18:44 +0800

    Create Dependency_injection.cs

---
+++ CSharpNotes/Dependency_injection.cs
@@ -0,0 +1,74 @@
+using System;
+
+
+namespace AppForInterview
+{
+    public interface IAccount
+    {
+        void PrintData();
+    }
+
+    public class SavingAccount : IAccount
+    {
+        public void PrintData()
+        {
+            Console.WriteLine("Saving account data.");
+        }
+    }
+
+    public class CurrentAccount : IAccount
+    {
+        public void PrintData()
+        {
+            Console.WriteLine("Current account data.");
+        }
+    }
+
+    public class Account
+    {
+        // No need to be dependent on SavingAccount & CurrentAccount classes
+        // Code below is tightly coupled:
+        //
+        // SavingAccount savingAccount = new SavingAccount();
+        // CurrentAccount currentAccount = new CurrentAccount();
+
+        // public void PrintAccounts()
+        // {
+        //    savingAccount.PrintData();
+        //    currentAccount.PrintData();
+        // }
+
+
+        //----------------DI via constructor-------------------------------------
+        // For loosely coupled code use Interface
+        // so that no class objects are created in class
+        private IAccount _account1, _account2;
+
+        public Account(IAccount account1, IAccount account2)
+        {
+            this._account1 = account1;
+            this._account2 = account2;
+        }
+
+        public void PrintAccounts()
+        {
+            _account1.PrintData();
+            _account2.PrintData();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAccount savingAccount = new SavingAccount();
+            IAccount currentAccount = new CurrentAccount();
+
+            Account account = new Account(savingAccount, currentAccount);
+            account.PrintAccounts();
+
+            Console.Read();
+        }
+    }
+}
