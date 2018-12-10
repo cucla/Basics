6beb305b262865a41e29895a3749ccc77759ff35
cucla <cucla@users.noreply.github.com>
2018-12-10 17:39:07 +0800

    Update Big_O.cpp

--- CPPNotes/Big_O.cpp
+++ CPPNotes/Big_O.cpp
@@ -9,10 +9,14 @@
 const size_t SIZE = 15;
 
 void print(int * arr, size_t n);
-int linearSearch(int * arr, size_t n, int key);		// O(n)
+int linearSearch(int * arr, size_t n, int key);			// O(n)
 int binarySearch(int * arr, int l, int r, int key);		// O(log_n)
-void selectionSort(int * arr, size_t n);			// O(n^2)
-void insertionSort(int * arr, size_t n);
+void insertionSort(int * arr, size_t n);				// O(n^2)
+void selectionSort(int * arr, size_t n);				// O(n^2)
+
+void merger(int * arr, int lo, int  mi, int hi);
+void mergeSortHelper(int * arr, int lo, int hi);
+void mergeSort(int * arr, int arr_size);				// O(nlog_n)
 
 
 int main() {
@@ -22,12 +26,16 @@ int main() {
 	srand(time(NULL));
 	for (size_t i = 0; i < SIZE; i++) {
 		arr[i] = rand() % 100;
-		//std::cout << arr[i] << " ";
+		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
 
-	insertionSort(arr, SIZE);
-	print(arr, SIZE);
+	int arr3[7] = { 7, 6, 5, 4, 3, 2, 1 };
+
+	mergeSort(arr3, 7);
+	print(arr3, 7);
+
+
 
 
 	std::cin.get();
@@ -35,7 +43,7 @@ int main() {
 
 void print(int * arr, size_t n) {
 	for (size_t i = 0; i < n; i++) {
-		std::cout << arr[i] << " ";
+		std::cout << arr[i] << " " << std::flush;
 	}
 }
 
@@ -51,17 +59,20 @@ int linearSearch(int * arr, size_t n, int key) {
 
 // 'binarySearch' -> O(log_n), logarithmic runtime
 // each iteration eliminates half of the array
-// repeatedly dividing 1023 by 2 will yield at most 10 divisions (2^10 = 1023)
-// For any number n, lon2 n  gives the number of times n needs to be divided by 2 before reaching 1 
+// For any number n, lon2 n  gives the number of times n needs to be divided by 2 before reaching 1
 int binarySearch(int * arr, int l, int r, int key) {
 	if (r >= l) {
 		int mid = l + (r - l) / 2;
+
 		if (arr[mid] == key)
 			return mid;
+
 		if (arr[mid] > key)
 			return binarySearch(arr, l, mid - 1, key);
+
 		return binarySearch(arr, mid + 1, r, key);
 	}
+
 	return -1;
 }
 
@@ -71,11 +82,23 @@ void swap(int * a, int * b) {
 	*b = tmp;
 }
 
+// 'insertionSort' -> O(n^2), quadratic runtime
+void insertionSort(int * arr, size_t n) {
+	for (size_t i = 1; i < n; i++) {
+		for (size_t j = i - 1; j-- > 0;) {
+			if (arr[j + 1] < arr[j]) {
+				swap(&arr[j + 1], &arr[j]);
+			}
+		}
+	}
+}
+
 // 'selectionSort' -> O(n^2), quadratic runtime
 void selectionSort(int * arr, size_t n) {
 	int min;
 	for (auto i = 0; i < n - 1; i++) {
 		min = i;
+
 		for (auto j = i + 1; j < n; j++) {
 			if (arr[j] < arr[min]) 
 				min = j;
@@ -84,14 +107,47 @@ void selectionSort(int * arr, size_t n) {
 	}
 }
 
-// 'insertionSort'
-void insertionSort(int * arr, size_t n) {
-	for (size_t i = 1; i < n; i++) {
-		for (size_t j = i - 1; j-- > 0;) {
-			if (arr[i] < arr[j])
-				swap(&arr[i], &arr[i - 1]);
-		}
+// 'mergeSort' -> O(nlog_n), logarithmic runtime
+// divide-and-conquer sorting algorithm
+// always divides the array in two halves and take linear time to merge two halves
+void merger(int * arr, int leftIdx, int  mi, int rightIdx) {
+	int * temp = new int[rightIdx - leftIdx + 1];
+
+	int i = leftIdx;	// Initial index of left subarray
+	int j = mi + 1;		// Initial index of right subarray
+	int k = 0;			// Initial index of merged subarray
+
+	while (i <= mi && j <= rightIdx) {
+		if (arr[i] <= arr[j])
+			temp[k++] = arr[i++];
+		else
+			temp[k++] = arr[j++];
 	}
 
+	//rest elements of left-half
+	while (i <= mi)
+		temp[k++] = arr[i++];
+	//rest elements of right-half
+	while (j <= rightIdx)
+		temp[k++] = arr[j++];
+
+	//copy the mergered temporary array to the original array
+	for (k = 0, i = leftIdx; i <= rightIdx; ++i, ++k)
+		arr[i] = temp[k];
+
+	delete[] temp;
+}
+void mergeSortHelper(int * arr, int leftIdx, int rightIdx) {
+	int mid;
+	if (leftIdx < rightIdx) {
+		mid = leftIdx + (rightIdx - leftIdx) / 2;
+
+		mergeSortHelper(arr, leftIdx, mid);
+		mergeSortHelper(arr, mid + 1, rightIdx);
+		merger(arr, leftIdx, mid, rightIdx);
+	}
+}
+void mergeSort(int * arr, int n) {
+	mergeSortHelper(arr, 0, n - 1);
 }
 
