63884efe5667bfc7de072be30a9a4862c1cce826
cucla <cucla@users.noreply.github.com>
2018-12-26 17:04:30 +0800

    Create Runtime_iterative.cpp

---
+++ CPPreview/Runtime_iterative.cpp
@@ -0,0 +1,102 @@
+// Example 1, O(n)
+
+   x = n
+   while ( x > 0 ) {
+       x = x - 1
+   }
+   
+// Example 2, O(log_n)
+
+   x = n
+   while ( x > 0 ) {
+       x = x / 2
+   }
+   
+// Example 3, O(n^2)
+
+   x = n
+   while ( x > 0 ) {
+      y = n
+      while ( y > 0 ) {
+          y = y - 1
+      }
+      x = x - 1
+   }
+   
+// Example 4, O(n^2)
+
+   x = n
+   while ( x > 0 ) {
+      y = x
+      while ( y > 0 ) {
+          y = y - 1
+      }
+      x = x - 1
+   }
+   
+// Example 5, O(n * log_n)
+
+   x = n
+   while ( x > 0 ) {
+      y = n
+      while ( y > 0 ) {
+          y = y / 2
+      }
+      x = x - 1
+   }
+   
+// Example 6, O(n * log_n)
+
+   x = n
+   while ( x > 0 ) {
+      y = x
+      while ( y > 0 ) {
+          y = y / 2
+      }
+      x = x - 1
+   }
+   
+// Example 7, O(n * log_n)
+
+   x = n
+   while ( x > 0 ) {
+      y = n
+      while ( y > 0 ) {
+          y = y - 1
+      }
+      x = x / 2
+   }
+   
+// Example 8, O(n)
+
+   x = n
+   while ( x > 0 ) {
+      y = x
+      while ( y > 0 ) {
+          y = y - 1
+      }
+      x = x / 2
+   }
+   
+// Example 9, O(log^2_n)
+
+   x = n
+   while ( x > 0 ) {
+      y = n
+      while ( y > 0 ) {
+          y = y / 2
+      }
+      x = x / 2
+   }
+   
+// Example 10, O(log^2_n)
+
+   x = n
+   while ( x > 0 ) {
+      y = x
+      while ( y > 0 ) {
+          y = y / 2
+      }
+      x = x / 2
+   }
+   
