94a0af8fdb719f6f20889e3769e118124f31cb86
cucla <cucla@users.noreply.github.com>
2018-12-31 23:10:03 +0800

    Create 701_Insert_into_BST.cpp

---
+++ CPPreview/Solutions/701_Insert_into_BST.cpp
@@ -0,0 +1,20 @@
+//LeetCode 701
+//Given the root node of a binary search tree (BST) and a value to be inserted into the tree, 
+//insert the value into the BST. Return the root node of the BST after the insertion
+
+    TreeNode * insertIntoBST(TreeNode * root, int val) {
+        if (val >= root->val) {
+            if (root->right) 
+                insertIntoBST(root->right, val);
+            else
+                root->right = new TreeNode{ val };
+        }
+        else {
+            if (root->left) 
+                insertIntoBST(root->left, val);
+            else
+                root->left = new TreeNode{ val };
+        }
+        return root;
+    }
+    
