f71e4929c8a03b8d0a77e16f5bb738d17cba19b0
cucla <cucla@users.noreply.github.com>
2019-01-01 12:28:51 +0800

    Update 701_Insert_into_BST.cpp

--- CPPreview/Solutions/701_Insert_into_BST.cpp
+++ CPPreview/Solutions/701_Insert_into_BST.cpp
@@ -2,6 +2,13 @@
 //Given the root node of a binary search tree (BST) and a value to be inserted into the tree, 
 //insert the value into the BST. Return the root node of the BST after the insertion
 
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
     TreeNode * insertIntoBST(TreeNode * root, int val) {
         if (val >= root->val) {
             if (root->right) 
