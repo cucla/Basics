3b1c388fb9064bb21c0df6f3a31dc363bcc3d68a
cucla <cucla@users.noreply.github.com>
2019-01-11 16:16:39 +0800

    Create Bit_manipulation_02.cpp

---
+++ CPPreview/Bit_manipulation_02.cpp
@@ -0,0 +1,46 @@
+/*  Bitwise Operators:
+ *
+ *  NOT ( ~ ):  1's complement, flip 0 to 1; 1 to 0
+char a = 0xA5;		// 1010.0101
+char b = ~a;      // 0101.1010
+ *
+ *  AND ( & ):  operates on two equal-length bit patterns; 1 & 1 = 1, otherwise 0
+char x = 75;      // 0100.1011
+char y = 82;      // 0101.0010
+int r = x & y;    // 0100.0010
+ *
+ *  OR ( | ):   operates on two equal-length bit patterns; 0 | 0 = 0, otherwise 1
+char x = 75;      // 0100.1011
+char y = 82;      // 0101.0010
+int r = x | y;    // 0101.1011
+ *
+ *  XOR ( ^ ):  takes two equal-length bit patterns; 1 ^ 0 = 1; 0 ^ 1 = 1, otherwise 0
+char x = 75;      // 0100.1011
+char y = 82;      // 0101.0010
+int r = x ^ y;    // 0001.1001
+ *
+ *  Left Shift ( << ): appends 0s at the end; is equivalent to multiplying with 2^k ( if we are shifting k bits )
+char x = 75;      // 0100.1011
+char r = x << 4;  // 1011.0000
+ *
+ *  Right Shift ( >> ): appends 0s at the beginning;
+char x = 75;      // 0100.1011
+char r = x >> 4;  // 0000.0100
+ *
+ */
+ 
+ // 1. Check if a given number is a power of 2
+ 
+ // 1. Check if a given number is a power of 2-------------------------------------------------
+ // x = 4 = (100)2
+ // x - 1 = 3 = (011)2 
+ // x & (x-1) = 4 & 3 = (100)2 & (011)2 = (000)2
+ 
+ bool isPowerOfTwo(int x) {
+	// x will check if x == 0 and !(x & (x - 1)) will check if x is a power of 2 or not
+	return (x && !(x & (x - 1)));
+ }
+ 
+ //-------------------------------------------------------------------------------------------
+ //-------------------------------------------------------------------------------------------
+ //-------------------------------------------------------------------------------------------
