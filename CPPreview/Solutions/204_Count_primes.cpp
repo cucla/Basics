3ae1e258d2a0f9c8d68476aadd60da6f1885718c
cucla <cucla@users.noreply.github.com>
2019-01-12 14:31:28 +0800

    Create 204_Count_primes.cpp

---
+++ CPPreview/Solutions/204_Count_primes.cpp
@@ -0,0 +1,19 @@
+//LeetCode 204
+//Count the number of prime numbers less than a non-negative number, n
+
+int countPrimes(int n) {
+	if (n <= 2) return 0;
+	std::vector<bool> passed(n, false);
+	int sum = 1;
+	int upper = sqrt(n);
+	for (int i = 3; i<n; i += 2) {
+		if (!passed[i]) {
+			sum++;
+			if (i>upper) continue;
+			for (int j = i * i; j<n; j += i) {   // non prime numbers are formed from n * n of the first prime numbers and consecutive +n steps
+				passed[j] = true;
+			}
+		}
+	}
+	return sum;
+}
