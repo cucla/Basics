6f81641a33b7b4a18841dbd194ed91bfdac1fe67
cucla <cucla@users.noreply.github.com>
2019-01-28 17:56:24 +0800

    Update overlapping_intervals.cpp

--- CPPreview/Solutions/overlapping_intervals.cpp
+++ CPPreview/Solutions/overlapping_intervals.cpp
@@ -94,6 +94,28 @@ int totalNInt(std::vector<std::pair<int, int>> & v1)
 	return v3.size();
 }
 
+//-----------merging vactor of structs------------------
+struct Interval {
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
+
+std::vector<Interval> merge(std::vector<Interval> & ins) {
+	if (ins.empty()) 
+		return std::vector<Interval>{};
+	std::vector<Interval> res;
+	sort(ins.begin(), ins.end(), [](Interval a, Interval b) {return a.start < b.start; });
+	res.push_back(ins[0]);
+	for (int i = 1; i < ins.size(); i++) {
+		if (res.back().end < ins[i].start) res.push_back(ins[i]);
+		else
+			res.back().end = std::max(res.back().end, ins[i].end);
+	}
+	return res;
+}
+
 
 int main() 
 {	 
