c1318cc8be511675cb552e49b34199a20e85391b
cucla <cucla@users.noreply.github.com>
2019-04-12 11:21:55 +0800

    Update Buy_Sell_Stock.cpp

--- CPPreview/Solutions/Buy_Sell_Stock.cpp
+++ CPPreview/Solutions/Buy_Sell_Stock.cpp
@@ -4,14 +4,14 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 Note: You may not engage in multiple transactions at the same time (i.e., you must sell 
 the stock before you buy again). */
     
-    int maxProfit(vector<int> & prices) {
-        int maxprofit = 0;
-        for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] > prices[i - 1])
-                maxprofit += prices[i] - prices[i - 1];
-        }
-        return maxprofit;
-    }
+int maxProfit(vector<int> & prices) {
+	int maxprofit = 0;
+	for (int i = 1; i < prices.size(); i++) {
+	    if (prices[i] > prices[i - 1])
+		maxprofit += prices[i] - prices[i - 1];
+	}
+	return maxprofit;
+}
     
 /* II. Maximum profit earned for at most K transactions
 Note: You may not engage in multiple transactions at the same time (i.e., you must sell 
@@ -19,7 +19,6 @@ the stock before you buy again). */
 
 int maxProfit(vector<int> & prices, int k) {
 	std::priority_queue<int, std::vector<int>, std::greater<int>> maxHeap;
-
 	int maxprofit = 0;
 	for (int i = 1; i < prices.size(); i++) {
 		if (prices[i] > prices[i - 1])
@@ -29,13 +28,10 @@ int maxProfit(vector<int> & prices, int k) {
 			maxprofit = 0;
 		}
 	}
-	maxHeap.push(maxprofit);
-
-	maxprofit = 0;
+	maxHeap.push(maxprofit); maxprofit = 0;
 	for (int i = 0; i < k; i++) {
 		if (!maxHeap.empty())
 			maxprofit += maxHeap.top(); maxHeap.pop();
 	}
-
 	return maxprofit;
 }
