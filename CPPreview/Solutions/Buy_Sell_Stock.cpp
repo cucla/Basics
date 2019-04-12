20c481eba3f11ae093fe66381ea21215bba3a94c
cucla <cucla@users.noreply.github.com>
2019-04-12 17:34:15 +0800

    Update Buy_Sell_Stock.cpp

--- CPPreview/Solutions/Buy_Sell_Stock.cpp
+++ CPPreview/Solutions/Buy_Sell_Stock.cpp
@@ -34,21 +34,3 @@ int maxProfit(vector<int> & prices) {
 Note: You may not engage in multiple transactions at the same time (i.e., you must sell 
 the stock before you buy again). */
 
-int maxProfit(vector<int> & prices, int k) {
-	std::priority_queue<int, std::vector<int>, std::greater<int>> maxHeap;
-	int maxprofit = 0;
-	for (int i = 1; i < prices.size(); i++) {
-		if (prices[i] > prices[i - 1])
-			maxprofit += prices[i] - prices[i - 1];
-		if ((prices[i] < prices[i - 1]) && maxprofit != 0) {
-			maxHeap.push(maxprofit);
-			maxprofit = 0;
-		}
-	}
-	maxHeap.push(maxprofit); maxprofit = 0;
-	for (int i = 0; i < k; i++) {
-		if (!maxHeap.empty())
-			maxprofit += maxHeap.top(); maxHeap.pop();
-	}
-	return maxprofit;
-}
