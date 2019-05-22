a2a3c73405a51afd99b36efed38a806c5d566828
cucla <cucla@users.noreply.github.com>
2019-05-22 10:41:54 +0800

    Create CURSOR.sql

---
+++ SQLNotes/CURSOR.sql
@@ -0,0 +1,26 @@
+-- Declaring the Student cursor to rename all tables by adding ‘_backup’ 
+
+USE UniversityV3
+GO
+
+DECLARE @TableName VARCHAR(50),
+        @NewTableName VARCHAR(50)
+
+DECLARE Student_Cursor CURSOR FOR 
+SELECT T.TABLE_NAME FROM INFORMATION_SCHEMA.TABLES T;
+
+OPEN Student_Cursor
+FETCH NEXT FROM Student_Cursor INTO @TableName
+WHILE @@FETCH_STATUS = 0
+BEGIN
+  IF RIGHT(@TableName, 6) <> 'Backup'
+  BEGIN
+    SET @NewTableName = @TableName+'_Backup'
+    EXEC sp_rename @TableName, @NewTableName
+  END
+ELSE
+PRINT 'Backup table name already exists: '+@TABLENAME
+FETCH NEXT FROM Student_Cursor INTO @TableName
+END
+CLOSE Student_Cursor 
+DEALLOCATE Student_Cursor 
