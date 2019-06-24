001f0391080ea5708133bc334af8ba0cd5749093
cucla <cucla@users.noreply.github.com>
2019-06-24 17:56:14 +0800

    Update HASH_function.cpp

--- CPPreview/HASH_function.cpp
+++ CPPreview/HASH_function.cpp
@@ -13,12 +13,11 @@
 */
 
 
-// 1: class with operator==(), specialized std::hash<Node> template in common namespace
-//		std::unordered_map<Node, int> m
-// 2: class with operator==(), custom HashFunction template
-//		std::unordered_map<Node, int, HashFunction<Node>> m
-// 3: using lambda expressions
-//		std::unordered_map<Node, int, decltype(hash), decltype(equal)> m(8, hash, equal)
+// 1: specialized struct std::hash<Node>:	std::unordered_map<Node, int> m
+// 2: using custom HashFunction template:	std::unordered_map<Node, int, HashFunction<Node>> m
+// 3: using lambda expressions:			std::unordered_map<Node, int, decltype(hash), decltype(equal)> m(8, hash, equal)
+// 4. std::pair as a KEY: 			std::unordered_map<std::pair<int, int>, Node*> m;
+// 5. Classic Knuth 32-bit combiner for 3 integers
 
 
 // 1: class with operator==(), specialized std::hash<Node> template in common namespace
@@ -198,5 +197,59 @@ NODE: 1000-thousand
 */
 
 
+// 4. std::pair as a KEY
+/* You can use hash_combine as the basis for many other things, like tuples and ranges, so you could hash 
+ * an entire (ordered) container, for example, as long as each member is individually hashable.
+ */
+class Node {
+public:
+	Node() {}
+	Node(int A, std::string B) : a{ A }, b{ B } {}
+
+	void print(std::ostream & os = std::cout) const {
+		os << "NODE: " << a << b << "\n";
+	}
+private:
+	int a;
+	std::string b;
+};
+
+template <typename T>
+inline void hash_combine(std::size_t & seed, const T & v) {
+	std::hash<T> hasher;
+	seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template<typename F, typename S>
+struct std::hash<std::pair<F, S>> {
+	std::size_t operator()(const std::pair<F, S>& some_pair) const {
+		size_t seed = 0;
+		hash_combine(seed, some_pair.first);
+		hash_combine(seed, some_pair.second);
+		return seed;
+	}
+};
+
+int main() {
+	std::unordered_map<std::pair<int, int>, Node*> m;
+	m.emplace(std::make_pair(1, 1), new Node(100, "-hundred"));
+	m.emplace(std::make_pair(2, 2), new Node(1000, "-thousand"));
+
+	for (auto el : m) {
+		std::cout << el.first.first << std::endl;
+		el.second->print(); 
+	}
+}
 
 
+// 5. Classic Knuth 32-bit combiner for 3 integers
+int hash(int v1, int v2, int v3)
+{
+  int v = v1;
+  v *= 2654435741;
+  v += v2;
+  v *= 2654435761;
+  v ^= v3;
+  v *= 2654435789;
+  return v;
+}
