8b5f856b4d9d7189f919a13b3fc20fca28733181
cucla <cucla@users.noreply.github.com>
2019-07-05 17:28:48 +0800

    Update 16_when_ctor_called.cpp

--- CPPreview/OOP/16_when_ctor_called.cpp
+++ CPPreview/OOP/16_when_ctor_called.cpp
@@ -25,26 +25,26 @@ public:
 
 int main() 
 {
-	Foo var;				// ctor
-	Foo arr[2];				// ctor x2
-	vector<Foo> vect(2);	// ctor x2
+	Foo var;			// ctor
+	Foo arr[2];			// ctor x2
+	vector<Foo> vect(2);		// ctor x2
 
-	Bar b(var);				// 1. copy ctor, to generate instance of Foo class
-							// 2. Bar ctor
+	Bar b(var);			// 1. copy ctor, to generate instance of Foo class
+					// 2. Bar ctor
 
 	Foo jar(var);			// copy ctor
 	Foo car = var;			// copy ctor
 	car = arr[1];			// copy assignment operator
-	Foo star = std::move(jar);			// move ctor
-	Foo costar(std::move(star));		// move ctor
+	Foo star = std::move(jar);	// move ctor
+	Foo costar(std::move(star));	// move ctor
 
 	Foo* varptr;			// nothing
-	varptr = new Foo;					// ctor
-	Foo* darr = new Foo[2];				// ctor x2	
+	varptr = new Foo;		// ctor
+	Foo* darr = new Foo[2];		// ctor x2	
 
-	Foo a;					// ctor
+	Foo a;				// ctor
 	Foo* aptr = &a;			// nothing
-							// no need to call 'delete' on a pointer that wasn't initialized with 'new'
+					// no need to call 'delete' on a pointer that wasn't initialized with 'new'
 
 	// "unique_ptr to an array", NOT array of std::unique_ptr
 	std::unique_ptr<Foo[]> my_array1(new Foo[2]);			// ctor x2, calls 'delete[]'
@@ -55,15 +55,15 @@ int main()
 	// my_array4.get()[0]  , to get element
 
 	unique_ptr<Foo> my_array2[3];
-	for (auto& p : my_array2)			// ctor x3	
+	for (auto& p : my_array2)	// ctor x3	
 		p = make_unique<Foo>();
 	
 	Foo* varptr2;			// nothing
 	varptr2 = varptr;		// nothing
-	varptr2 = new Foo;					// ctor
-	varptr2 = new Foo(darr[0]);			// copy ctor
-	std::cout << "--------------" << std::endl;
+	varptr2 = new Foo;			// ctor
+	varptr2 = new Foo(darr[0]);		// copy ctor
 
+	
 	delete varptr;
 	delete[] darr;
 	delete varptr2;
